0be4f081a5943a99a51cf32a6a363114234963c0
tess-phillips <124443397+tess-phillips@users.noreply.github.com>
2023-07-06 19:56:13 +0100

    about structure simplified

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ import { getModal } from './helpers/getModal.js';
 document.addEventListener('DOMContentLoaded', function() {
   // Load modal.js
   getModal("myResModal", "myResBtn", "myResClose");
-  getModal("myWhenModal", "myWhenBtn", "myWhenClose");
-  getModal("myAppModal", "myAppBtn", "myAppClose");
-  getModal("myWeatherModal", "myWeatherBtn", "myWeatherClose");
+  // getModal("myWhenModal", "myWhenBtn", "myWhenClose");
+  // getModal("myAppModal", "myAppBtn", "myAppClose");
+  // getModal("myWeatherModal", "myWeatherBtn", "myWeatherClose");
   // I FINALLY GOT WINDOW TO WORK FOR EACH BUTTTT
   // I CANT GET SPAN TO WORK ON EACH
 });
