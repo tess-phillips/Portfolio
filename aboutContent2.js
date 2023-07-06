624ffc591d317f91a0eac41f744460f9e6c649b8
tess-phillips <124443397+tess-phillips@users.noreply.github.com>
2023-07-06 20:27:24 +0100

    structure so much better

---
+++ aboutContent2.js
@@ -0,0 +1,34 @@
+// import { createElement, appendElements } from './helpers/createElements.js';
+// import { createContainerWithTextOnly, createContainerWithImages, createContainerWithOther } from "./helpers/createContainers.js";
+// // import { text2innerHTML } from './helpers/text2innerHTML.js';
+
+// // Create the first half
+// // const firstHalf = createElement('div', { className: 'half' });
+// const box1 = createContainerWithTextOnly('h3','quarter box1', 'Recent Education & Experience', [
+//   "I'm currently working on <b>Founders and Coders</b> software development bootcamp",
+//   "I'm learning: Web development fundamentals, front and back-end development, workflows, databases....",
+//   "I hold a Bachelor's degree in <b>Mathematics</b> from University of Manchester"
+// ]);
+// const box2 = createContainerWithImages('h3','quarter box2','Languages and Tools:', [
+//   ['https://www.w3schools.com/css/', 'css3','https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg'],
+//   ['https://www.w3.org/html/', 'html5',"https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"],
+//   ['https://developer.mozilla.org/en-US/docs/Web/JavaScript', 'javascript',"https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"],
+//   ['https://www.mysql.com/', 'mysql',"https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" ],
+//   ['https://nodejs.org', 'nodejs',"https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"],
+//   ['https://www.postgresql.org', 'postgresql',"https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg"],
+//   ['https://www.python.org', 'python',"https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg"],
+//   ['https://reactjs.org/', 'react',"https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"]
+// ]);
+
+// const box4 = createContainerWithTextOnly('h3','quarter box4', 'Strengths & Passions', [
+//   "I'm a passionate and <b>curious learner</b> who thrives in collaborative environments",
+//   "I believe in the power of technology to create <b>positive change</b>.",
+//   "I have a deep love for the <b>environment </b>and a strong commitment to sustainability",
+//   "I enjoy tackling challenging problems and finding <b>creative</b> solutions."
+// ]);
+
+// // appendElements(secondHalf, [box3, box4]);
+
+// // // Append the halves to the section
+// // const section = document.getElementById('about-placeholder');
+// // appendElements(section, [firstHalf, secondHalf]);
--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import { getModal } from './helpers/getModal.js';
+import { createContainerWithTextOnly, createContainerWithImages, createContainerWithOther } from "./helpers/createContainers.js";
+
 
 document.addEventListener('DOMContentLoaded', function() {
   // Load modal.js
@@ -9,3 +11,28 @@ document.addEventListener('DOMContentLoaded', function() {
   // I FINALLY GOT WINDOW TO WORK FOR EACH BUTTTT
   // I CANT GET SPAN TO WORK ON EACH
 });
+
+const box1 = createContainerWithTextOnly('h3','quarter box1', 'Recent Education & Experience', [
+  "I'm currently working on <b>Founders and Coders</b> software development bootcamp",
+  "I'm learning: Web development fundamentals, front and back-end development, workflows, databases....",
+  "I hold a Bachelor's degree in <b>Mathematics</b> from University of Manchester"
+]);
+const box2 = createContainerWithImages('h3','quarter box2','Languages and Tools:', [
+  ['https://www.w3schools.com/css/', 'css3','https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg'],
+  ['https://www.w3.org/html/', 'html5',"https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"],
+  ['https://developer.mozilla.org/en-US/docs/Web/JavaScript', 'javascript',"https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"],
+  ['https://www.mysql.com/', 'mysql',"https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" ],
+  ['https://nodejs.org', 'nodejs',"https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"],
+  ['https://www.postgresql.org', 'postgresql',"https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg"],
+  ['https://www.python.org', 'python',"https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg"],
+  ['https://reactjs.org/', 'react',"https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"]
+]);
+const box4 = createContainerWithTextOnly('h3','quarter box4', 'Strengths & Passions', [
+  "I'm a passionate and <b>curious learner</b> who thrives in collaborative environments",
+  "I believe in the power of technology to create <b>positive change</b>.",
+  "I have a deep love for the <b>environment </b>and a strong commitment to sustainability",
+  "I enjoy tackling challenging problems and finding <b>creative</b> solutions."
+]);
+document.querySelector('.box1').append(box1)
+document.querySelector('.box2').append(box2)
+document.querySelector('.box4').append(box4)
