cc978c8fa6e04e2a717c14c5670f0f9ea71d2f83
tess-phillips <124443397+tess-phillips@users.noreply.github.com>
2023-07-07 11:15:59 +0100

    removed slide container since its not used anymore

--- helpers/text2innerHTML.js
+++ helpers/text2innerHTML.js
@@ -1,8 +1,8 @@
-/**
- * 
- * @param {*} array [link,tag,innerHTML]
- * @returns 
- */
-export const text2innerHTML = (array) => {
-    return array.map(elt => elt = `<${elt[1]} href="${elt[0]}">${elt[2]}</${elt[1]}>`)
-}
+// /**
+//  * 
+//  * @param {*} array [link,tag,innerHTML]
+//  * @returns 
+//  */
+// export const text2innerHTML = (array) => {
+//     return array.map(elt => elt = `<${elt[1]} href="${elt[0]}">${elt[2]}</${elt[1]}>`)
+// }
--- helpers/toggleElements.js
+++ helpers/toggleElements.js
@@ -1,16 +1,16 @@
-/**
- * 
- * @param {*} elementId 
- * @param {*} btnid 
- */
-export const toggle = (elementId,btnid) => {
-    const element = document.getElementById(elementId);
-    const btn = document.getElementById(btnid);
-    if (element.style.display == "none") {
-      element.style.display = "block";
-      btn.innerHTML="Hide resume";
-    } else {
-      element.style.display = "none";
-      btn.innerHTML="Show resume"
-    }
-}
+// /**
+//  * 
+//  * @param {*} elementId 
+//  * @param {*} btnid 
+//  */
+// export const toggle = (elementId,btnid) => {
+//     const element = document.getElementById(elementId);
+//     const btn = document.getElementById(btnid);
+//     if (element.style.display == "none") {
+//       element.style.display = "block";
+//       btn.innerHTML="Hide resume";
+//     } else {
+//       element.style.display = "none";
+//       btn.innerHTML="Show resume"
+//     }
+// }
