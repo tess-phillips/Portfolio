f6d58f36343f32644c9568c92bdc245c5626236d
tess-phillips <124443397+tess-phillips@users.noreply.github.com>
2023-07-07 13:11:50 +0100

    adding projectContent.js file

---
+++ jsFiles/projectContent.js
@@ -0,0 +1,45 @@
+import {createCardUsingTemplate} from "../helpers/createCardUsingTemplate.js"
+
+const articles = [
+  {
+    title: "My Hobby Page",
+    summary:
+      "This site is an informative page about bouldering and other types of climbing. It is also the first site I ever made in HTML.",
+    image: "./imgFiles/hobbyLanding.jpg",
+    href: "https://tess-phillips.github.io/FAC1/",
+  },
+  {
+    title: "FAC application: Project Gallery",
+    summary:
+      "This website is a portal to each of the other projects I made for the Founders and Coders Bootcamp Application. \
+      These include my Hobby Page, Movie Data Site, a website about me and a game called Slidey.",
+    image: "./imgFiles/galleryLanding.jpg",
+    href: "https://tess-phillips.github.io/FAC2/",
+  },
+  {
+    title: "TD Weather App",
+    summary: 
+      "This app is designed to be responsive and uses postcode to generate a map and accurate sunrise-sunset timings, as well as a comprehensive daily and 5-day weather forecast. \
+      To gather this information, our app integrates three APIs: open-meteo.com, postcodes.io, and thunderforest.com.\
+      I made this with Deepa on the FAC Foundation.",
+      image: "./imgFiles/TDWeather.png",
+      href: "https://deepsdali.github.io/Weather-Forecast-Project/",
+},
+// {
+//   title: "To Do List",
+//   summary: 
+//     "",
+//     image: "/imgFiles/TDWeather.png",
+//     href: "https://tess-phillips.github.io/ToDoList/",
+// },
+// {
+//   title: "Wordle",
+//   summary: 
+//     "",
+//     image: "/imgFiles/TDWeather.png",
+//     href: "https://tess-phillips.github.io/Wordle/",
+// }
+]
+articles.reverse()
+
+articles.forEach(createCardUsingTemplate);
--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 import { getModal } from './helpers/getModal.js';
-import { createCardUsingTemplate } from './helpers/createCardUsingTemplate.js';
 
 document.addEventListener('DOMContentLoaded', function() {
   // Load modal.js
@@ -43,46 +42,3 @@ projectsBtn.addEventListener('click', () => {
   projectsStuff.style.display = "block";
 });
 
-const articles = [
-  {
-    title: "My Hobby Page",
-    summary:
-      "This site is an informative page about bouldering and other types of climbing. It is also the first site I ever made in HTML.",
-    image: "/imgFiles/hobbyLanding.jpg",
-    href: "https://tess-phillips.github.io/FAC1/",
-  },
-  {
-    title: "FAC application: Project Gallery",
-    summary:
-      "This website is a portal to each of the other projects I made for the Founders and Coders Bootcamp Application. \
-      These include my Hobby Page, Movie Data Site, a website about me and a game called Slidey.",
-    image: "/imgFiles/galleryLanding.jpg",
-    href: "https://tess-phillips.github.io/FAC2/",
-  },
-  {
-    title: "TD Weather App",
-    summary: 
-      "This app is designed to be responsive and uses postcode to generate a map and accurate sunrise-sunset timings, as well as a comprehensive daily and 5-day weather forecast. \
-      To gather this information, our app integrates three APIs: open-meteo.com, postcodes.io, and thunderforest.com.\
-      I made this with Deepa on the FAC Foundation.",
-      image: "/imgFiles/TDWeather.png",
-      href: "https://deepsdali.github.io/Weather-Forecast-Project/",
-},
-// {
-//   title: "To Do List",
-//   summary: 
-//     "",
-//     image: "/imgFiles/TDWeather.png",
-//     href: "https://tess-phillips.github.io/ToDoList/",
-// },
-// {
-//   title: "Wordle",
-//   summary: 
-//     "",
-//     image: "/imgFiles/TDWeather.png",
-//     href: "https://tess-phillips.github.io/Wordle/",
-// }
-]
-articles.reverse()
-
-articles.forEach(createCardUsingTemplate);
