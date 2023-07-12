d77b0f9acc9056e6bb25508695e5f3896eff3691
tess-phillips <124443397+tess-phillips@users.noreply.github.com>
2023-07-12 08:24:38 +0100

    removed console.logs

--- helpers/getModal.js
+++ helpers/getModal.js
@@ -11,19 +11,15 @@ export const getModal = (myModal, myBtn, myClose) => {
 
   btn.onclick = function() {
     modal.style.display = "block";
-    console.log(modal.id, "btn");
   };
 
   span.addEventListener("click", function() {
     modal.style.display = "none";
-    console.log(modal.id)
-    console.log(modal.id, "span");
   });
 
   window.addEventListener("click", function(event) {
     if (event.target == modal) {
       modal.style.display = "none";
-      console.log(modal.id, "window");
     }
   });
 }
